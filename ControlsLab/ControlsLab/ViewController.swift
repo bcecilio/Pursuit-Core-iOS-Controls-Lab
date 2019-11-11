14f9b5e29026fa2d91dff6cb7114005f660668ac
brendon cecilio <brendon.cecilio@gmail.com>
2019-11-11 18:13:00 -0500

    for loops added to switch statements

--- ControlsLab/ControlsLab/ViewController.swift
+++ ControlsLab/ControlsLab/ViewController.swift
@@ -4,45 +4,67 @@ class ViewController: UIViewController {
     
     // MARK:- Outlets
     
-    @IBOutlet weak var numberLabel: UILabel!
-    @IBOutlet weak var suitSegementedController: UISegmentedControl!
+    @IBOutlet var numberLabel: [UILabel]!
     @IBOutlet weak var suitStepper: UIStepper!
-    @IBOutlet weak var suitImage: UIImageView!
-    
-    var suitNumbers: Int = 2 {
-        didSet {
-            numberLabel.text = "\(suitStepper.value)"
+    @IBOutlet var suitImage: [UIImageView]!
+    @IBOutlet weak var suitSegmentedController: UISegmentedControl!
+        
+    var suitNumbers: Int = 1 {
+        didSet{
+            for nums in numberLabel where suitNumbers < 14 {
+                if suitNumbers == 11 {
+                    nums.text = "J"
+                } else if suitNumbers == 12 {
+                    nums.text = "Q"
+                } else if suitNumbers == 13 {
+                    nums.text = "K"
+                } else if suitNumbers == 1 {
+                    nums.text = "A"
+                } else {
+                    nums.text = suitNumbers.description
+                }
+            }
         }
-    }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
     
     var currentSegmentIndex: Int = 0 {
         didSet {
-            switch suitSegementedController.selectedSegmentIndex {
+            switch suitSegmentedController.selectedSegmentIndex {
             case 0:
-                suitImage.image = UIImage.init(systemName: "suit.spade.fill")
+                for imageView in suitImage {
+                imageView.image = UIImage.init(systemName: "suit.spade.fill")
+                }
             case 1:
-                suitImage.image = UIImage.init(systemName: "suit.heart.fill")
+                for imageView in suitImage {
+                imageView.image = UIImage.init(systemName: "suit.heart.fill")
+                }
             case 2:
-                suitImage.image = UIImage.init(systemName: "suit.club.fill")
+                for imageView in suitImage {
+                imageView.image = UIImage.init(systemName: "suit.club.fill")
+                }
             case 3:
-                suitImage.image = UIImage.init(systemName: "suit.diamond.fill")
+                for imageView in suitImage {
+                imageView.image = UIImage.init(systemName: "suit.diamond.fill")
+                }
             default:
-                suitImage.image = #imageLiteral(resourceName: "blue_back")
+                for imageView in suitImage {
+                imageView.image = #imageLiteral(resourceName: "blue_back")
+                }
                 
             }
         }
     }
     
     func configureStepper() {
-        suitStepper.minimumValue = 1
-        suitStepper.maximumValue = 12
-        suitStepper.stepValue = 1
-        suitStepper.value = 2
+        suitStepper.minimumValue = Double(Int(1))
+        suitStepper.maximumValue = Double(Int(12))
+        suitStepper.stepValue = Double(Int(1))
+        suitStepper.value = Double(Int(2))
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
     }
     
     @IBAction func segmentedButtonChange(_ sender: UISegmentedControl) {
